a1a85e7d9210ca67917ef87914b11facd0ae5bb9
Padraig Gleeson <p.gleeson@gmail.com>
2018-09-07 18:10:05 +0100

    Improved analysis - requires latest pynml!

--- NML2_models/analyse_EGL19.sh
+++ NML2_models/analyse_EGL19.sh
@@ -1,4 +1,15 @@
 set -e
 
-pynml-channelanalysis -temperature 34 -minV -40  -maxV 80  -duration 600  -clampBaseVoltage -55  -clampDuration 580  -stepTargetVoltage 5  -erev 52.4  -caConc 0.001 -ivCurve EGL-19.channel.nml 
+pynml-channelanalysis -temperature 34 \
+                      -minV -40  \
+                      -maxV 80  \
+                      -duration 340  \
+                      -clampBaseVoltage -70  \
+                      -clampDelay 20  \
+                      -clampDuration 300  \
+                      -stepTargetVoltage 2  \
+                      -erev 51  \
+                      -caConc 0.001  \
+                      -ivCurve  \
+                      EGL-19.channel.nml 
 
